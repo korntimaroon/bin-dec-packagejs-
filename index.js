feb05847d8a2668702612b055c4b762922a88e1d
korntimaroon <86138908+korntimaroon@users.noreply.github.com>
2022-09-05 10:00:37 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+function binaryToDecimal(n) {
+	const bin = n
+	return parseInt(bin,2)
+}
+function decimalToBinary(n) {
+	const dec = n
+    return null
+}
+export default { binaryToDecimal, decimalToBinary }	
